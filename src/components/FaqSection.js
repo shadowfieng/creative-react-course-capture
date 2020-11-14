8ffa686974c6fe3839a74642c34ea5ab17a912d3
shadowfieng <shadowfieng@gmail.com>
2020-11-14 18:24:29 +0500

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>
+                Any Questions <span>FAQ</span>
+            </h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Cum, dolor.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Cum, dolor.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Cum, dolor.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What products Do You Offer? </h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Cum, dolor.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FaqSection;
